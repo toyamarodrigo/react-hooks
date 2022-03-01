cb1db9ea08e9499dbb4198e22fb77f346d3f0740
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2022-03-01 03:14:40 -0300

    exercise 1 2 hooks

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,19 +3,20 @@
 
 import * as React from 'react'
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+function Greeting({initialName = ""}) {
+  const [name, setName] = React.useState(initialName);
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    const value = event.target.value;
+    setName(value);
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -23,7 +24,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Rodri" />
 }
 
 export default App
